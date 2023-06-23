91be16654ca7991d56886f6c196d16c777138104
gilson0516 <gilson0516@gmail.com>
2023-06-24 06:49:46 +0900

    day8-1

---
+++ day8.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from datetime import time, datetime
+
+st.header('st.slider')
+
+st.subheader('Slider')
+age = st.slider('How old are you?',0,130,25)
+st.write("I'm ", age, 'yesrs old')
+
+st.subheader('Range slider')
+values = st.slider('Select a range of values', 0.0, 100.0, (25.0,75.0))
+st.write('Values:', values)
