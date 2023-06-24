ec4c0344394d6f82628064663e83811bce5af98d
gilson0516 <gilson0516@gmail.com>
2023-06-24 11:13:56 +0900

    day ~12

--- day10.py
+++ day10.py
@@ -7,3 +7,26 @@ option = st.selectbox(
     ('Blue','Red','Green')
 )
 st.write('Your favorite color is ', option)
+
+st.header('st.multiselect')
+
+options = st.multiselect(
+    'What are your favorite colors',
+    ['Green', 'Yellow', 'Red','Blue'],
+    ['Yellow', 'Red']
+)
+st.write('You selected:', options)
+
+st.header('st.checkbox')
+
+st.write('Way would you like to order?')
+icecream = st.checkbox('Ice cream')
+coffee = st.checkbox('Coffee')
+cola = st.checkbox('Cola')
+
+if icecream:
+    st.write("Great! here's some more :icecream:")
+if coffee:
+    st.write("Okay, here's some coffee :coffee:")
+if cola:
+    st.write("Here you go :coffee:")
